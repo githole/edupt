62a2ba711c16c3b7bf835efa5d5429203526c530
hole <gigahole@gmail.com>
2013-06-04 09:59:23 +0900

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -6,5 +6,5 @@ int main(int argc, char **argv) {
 	std::cout << "Path tracing renderer: edupt" << std::endl << std::endl;
 
 	// 640x480の画像、(2x2) * 4 sample / pixel
-	edupt::render(640, 480, 512);
+	edupt::render(640, 480, 4);
 }
