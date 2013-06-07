402257fab413c0d6d9d6fb1d406bf785f259aae2
hole <gigahole@gmail.com>
2013-06-07 09:32:49 +0900

    OpenMP無効化

--- render.h
+++ render.h
@@ -30,7 +30,7 @@ int render(const int width, const int height, const int samples, const int subsa
 	std::cout << width << "x" << height << " " << samples * (subsamples * subsamples) << " spp" << std::endl;
 
 	// OpenMP
-#pragma omp parallel for schedule(dynamic, 1) num_threads(6)
+//#pragma omp parallel for schedule(dynamic, 1) num_threads(4)
 	for (int y = 0; y < height; y ++) {
 		std::cerr << "Rendering (y = " << y << ") " << (100.0 * y / (height - 1)) << "%" << std::endl;
 
